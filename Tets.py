1484e08d4368aae50df5a023b53da41190b3d394
harshitgupta1998 <harshit101998@gmail.com>
2020-11-11 22:13:39 +0530

    New dataset

--- Tets.py
+++ Tets.py
@@ -1,1 +1,4 @@
-print("Test.py")
+
+#x = "1248 CC"
+#print(type(x))
+#print(str(x).strip('CC'))
--- app.py
+++ app.py
@@ -6,8 +6,9 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-@app.route('/',methods=['POST'])
+model = pickle.load(open('random_forest_regression_model.pkl', 'rb')) #model is loaded here.
+
+@app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
 
